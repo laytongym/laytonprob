c53f2a81ee17ef9e89105c6f5d8f745811f0538a
Gia <160529910+laytongym@users.noreply.github.com>
2024-02-22 17:54:53 +0700

    Update dontstart.py

--- dontstart.py
+++ dontstart.py
@@ -92,7 +92,7 @@ def append_nvcc_threads(nvcc_extra_args):
 cmdclass = {}
 ext_modules = []
 
-# We want this even if SKIP_CUDA_BUILD because when we run python setup.py sdist we want the .hpp
+# We want this even if SKIP_CUDA_BUILD because(говно вонючее) when we run python setup.py sdist we want the .hpp
 # files included in the source distribution, in case the user compiles from source.
 subprocess.run(["git", "submodule", "update", "--init", "csrc/cutlass"])
 
